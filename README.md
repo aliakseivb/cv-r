b61c7121cb32e2539c7c861d1d391175516126c2
AlexBoronin <alex.boronin.22@gmail.com>
2024-03-25 17:32:18 +0300

    init

---
+++ README.md
@@ -0,0 +1,54 @@
+# Aliaksei Baronin
+[![18971c8c254e0fb1b.th.png](https://i1.wampi.ru/2024/03/18/18971c8c254e0fb1b.th.png)](https://wampi.ru/image/Y5SoOql)
+## __Contacts__
+- __Location:__ Minsk, Belarus
+- __Phone:__ +375 (29) 782-41-71
+- __Email:__ alex.boronin.22@gmail.com
+- __GitHub:__ [aliakseivb](https://github.com/aliakseivb?tab=overview&from=2023-12-01&to=2023-12-31)
+- __TG:__ [aliakseivb](https://t.me/aliakseivb)
+- __LinkedIn:__ [Aliaksei Baronin](https://by.linkedin.com/in/aliaksei-baronin-450476252)
+---
+## **About me**
+I work well in a team, I have good communication skills, I am diligent and hardworking, and my perseverance, multiplied by the desire to learn and learn new things, will be excellent means to achieve the chosen goals.
+___ 
+## __Skills__
+
+- JavaScript
+- HTML/CSS(SCSS)
+- Angular
+- TS
+- Bootstrap
+- JQuery
+- Webpack/Gulp/Grunt
+- Python (basic)
+- GIT
+
+---
+## __Code example__
+``` 
+class Rsschool {
+    constructor(text) {
+        this.text = text
+        this.sumResume = 0
+    } 
+    upResume() {
+        this.sumResume += 1
+    }
+}
+const firstResume = new Rsschool('Resume text')
+```
+---
+## __Education__
+- __University:__ Academy of the Ministry of Internal Affairs of the Republic of Belarus, master of management
+- __Courses:__
+    - [RS Schools](https://rs.school/) - Course «JavaScript/Front-end. Stage 0»  (сompleted)
+    - [RS Schools](https://rs.school/) - Course «JavaScript/Front-end. Stage 1»
+    - [ItLogia](https://itlogia.ru/) - FE Developer
+    - [GeekBrains](https://gb.ru/) - Python Developer
+---
+## __Languages__
+- Belarusian - fluent
+- Russian - fluent
+- English - B1
+- Polish - I can speak and understand
+- Ukrainian - I can understand
---
+++ assets/scripts/script.js
@@ -0,0 +1,57 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const menu = document.querySelector('.navigation');
+  const burger = document.querySelector('.burger');
+  const closeBtn = document.querySelector('.close');
+  const body = document.querySelector('.body');
+  const back = document.querySelector('.back');
+  const header = document.querySelector('.header');
+  const year = document.querySelector('.year');
+  const sectionElements = document.querySelectorAll('.section')
+  //todo актуальный год
+  year.innerText = new Date().getFullYear();
+
+  //todo анимация хедера
+  window.onscroll = function () {
+    if (window.scrollY > 1) {
+      header.classList.add('shadow');
+    } else {
+      header.classList.remove('shadow');
+    }
+  };
+
+  //todo открытие меню
+  burger.addEventListener('click', () => {
+    menu.classList.add('open');
+    body.classList.add('hidden');
+    back.classList.add('show');
+  });
+
+  //todo закрытие меню
+  closeBtn.addEventListener('click', () => {
+    menu.classList.remove('open');
+    body.classList.remove('hidden');
+    back.classList.remove('show');
+  });
+
+  menu.addEventListener('click', (e) => {
+    if (menu.classList.contains('open')) {
+      if (e.currentTarget.contains(e.target) && !e.target.classList.contains('item')) {
+        return false
+      }
+      if (e.target.matches('span.item')) {
+        menu.classList.remove('open');
+        body.classList.remove('hidden');
+        back.classList.remove('show');
+        scrollToElem(e.target.dataset['scroll']);
+      }
+    } else {
+      if (e.target.matches('span.item')) {
+        scrollToElem(e.target.dataset['scroll']);
+      }
+    }
+  });
+  function scrollToElem(link) {
+    const toScrollElem = Array.from(sectionElements).find(elem => elem.id === link);
+    toScrollElem.scrollIntoView({ block: "center", behavior: "smooth" })
+  }
+});
